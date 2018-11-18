e906cb273fa01e408016014c1278b671817a56f5
jonas <jonas-gruempel@gmx.de>
2018-11-18 22:54:09 +0100

    v. 0.0.0.3

--- jsf.js
+++ jsf.js
@@ -1,4 +1,8 @@
 
+/*******************************************************************************
+* framework classes
+*******************************************************************************/
+
 class jsf {
 
     /**
@@ -33,7 +37,21 @@ class jsf {
     * returns an instance of jsfCookie
     */
     static get cookie () {
-       return new jsfCookie();
+      return new jsfCookie();
+    }
+
+    /**
+    * returns an instance of jsfJson
+    */
+    static get json () {
+      return new jsfJson();
+    }
+
+    /**
+    * returns an instance of jsfBrowser
+    */
+    static get browser () {
+      return new jsfBrowser();
     }
 
 }
@@ -50,23 +68,21 @@ class jsfXHTTP {
 
   /**
   * @param {string} path php-file path
-  * @param data xhttp-request data
+  * @param {array} data xhttp-request data
   * @param {function} success function which will be executed on success
   * @param {function} error function which will be executed on error
   * @param {function} stateChanged function which will be executed by a state change
   */
   post (path, data, success, error, stateChanged) {
 
-      var allreadySuccessed = false;
       var xhttp = new XMLHttpRequest();
 
       xhttp.onreadystatechange = function() {
         if (this.status == 200) {
           if (typeof success !== undefined && typeof success == 'function') {
-            if(!allreadySuccessed) {
-              allreadySuccessed = true;
-              return success(this);
-            }
+              if(this.resonseText != '' || this.response != '') {
+                return success(this);
+              }
           }
 
         } else if (this.status == 4) {
@@ -82,8 +98,18 @@ class jsfXHTTP {
 
       };
 
+      var requestString = '';
+
+      jsf.util.foreach(data, function(e, i) {
+        if (i != 0) {
+          requestString += '&';
+        }
+        requestString += e.toString();
+      });
+
       xhttp.open(_POST, path, true);
-      xhttp.send();
+      xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhttp.send(requestString);
 
   }
 
@@ -95,14 +121,12 @@ class jsfXHTTP {
   */
   get (url, success, error, stateChanged) {
 
-    var allreadySuccessed = false;
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
       if (this.status == 200) {
         if (typeof success !== undefined && typeof success == 'function') {
-          if(!allreadySuccessed) {
-            allreadySuccessed = true;
+          if(this.resonseText != '' || this.response != '') {
             return success(this);
           }
         }
@@ -134,12 +158,17 @@ class jsfXHTTP {
 
 class jsfHTML {
 
+  constructor() {
+    this.odom = new jsfDOM();
+    this.oevent = new jsfEvent();
+  }
+
   get dom () {
-    return new jsfDOM();
+    return this.odom;
   }
 
   get event () {
-    return new jsfEvent();
+    return this.oevent;
   }
 
 }
@@ -151,7 +180,7 @@ class jsfDOM {
   * @param {string} id html-element id
   */
   byId (id) {
-    return document.getElementById(id);
+    return new jsfElement(id);
   }
 
 }
@@ -197,6 +226,8 @@ class jsfEvent {
 
 class jsfMath {
 
+
+
 }
 
 
@@ -208,7 +239,7 @@ class jsfUtil {
   */
   foreach(array, func) {
     for (var i=0; i<array.length; i++) {
-      func(array[i]);
+      func(array[i], i);
     }
   }
 
@@ -254,6 +285,25 @@ class jsfUtil {
         }
       }
      return r;
+  }
+
+
+}
+
+class jsfJson {
+
+  /**
+  * @param {string} jsonString
+  */
+  parse(jsonString) {
+    return JSON.parse(jsonString);
+  }
+
+  /**
+  * @param {object} jsonObject
+  */
+  stringify(jsonObject) {
+    return JSON.stringify(jsonObject);
   }
 
 }
@@ -322,6 +372,122 @@ class jsfCookie {
 
 }
 
+class jsfElement {
+
+  constructor(id) {
+    this.get = document.getElementById(id);
+  }
+
+}
+
+class jsfBrowser {
+
+    /**
+    * goes back in browser history
+    */
+    goBack() {
+      window.history.back();
+    }
+
+    /**
+    * got to index of browser history
+    */
+    goToIndex(i) {
+      window.history.go(i);
+    }
+
+    /**
+    * get browser language
+    */
+    getLanguage() {
+      var userLang = navigator.language || navigator.userLanguage;
+    }
+
+    /**
+    * check if browser is mobile
+    */
+    isMobile() {
+      var check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+      return check;
+    }
+
+    /**
+    * check if browser is mobile/tablet
+    */
+    isMobileOrTablet() {
+      var check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+      return check;
+    }
+
+    /***************************************************************************
+    * following methods are successfully tested in:
+    ****************************************************************************
+    *   Firefox   0.8 - 61
+    *   Chrome    1.0 - 68
+    *   Opera     8.0 - 34
+    *   Safari    3.0 - 10
+    *   IE          6 - 11
+    ***************************************************************************/
+
+    /**
+    * testing if browser is Opera
+    */
+    isOpera() {
+      return (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    }
+
+    /**
+    * testing if browser is firefox
+    */
+    isFirefox() {
+      return (typeof InstallTrigger !== 'undefined');
+    }
+
+    /**
+    * testing if browser is safari
+    */
+    isSafari() {
+      return (/constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification)));
+    }
+
+    /**
+    * testing if browser is InternetExplorer
+    */
+    isInternetExplorer() {
+      return (/*@cc_on!@*/false || !!document.documentMode);
+    }
+
+    /**
+    * testing if browser is chrome
+    */
+    isChrome() {
+      return (!!window.chrome && !!window.chrome.webstore);
+    }
+
+    getBrowser() {
+      if(this.isOpera()) {
+        return 'opera';
+      } else if(this.isFirefox()) {
+        return 'firefox';
+      } else if(this.isSafari()) {
+        return 'safari';
+      } else if(this.isInternetExplorer()) {
+        return 'internet explorer';
+      } else if(this.isChrome()) {
+        return 'chrome';
+      } else {
+        return 'framework unknown browser';
+      }
+    }
+
+}
+
+/*******************************************************************************
+* other premade classes
+*******************************************************************************/
+
 /**
 * constants for jsfList-types
 **/
@@ -359,7 +525,7 @@ class jsfList {
     if(this.checkType(val)) {
       this.data.push(val);
     } else {
-
+      return new Error('parameter: false val');
     }
   }
 
@@ -367,6 +533,15 @@ class jsfList {
   * @param val value which will be removed from list
   */
   removeVal(val) {
+    var a = [];
+
+    this.foreach(function(e, i) {
+      if (e != val) {
+        a.push(e);
+      }
+    });
+
+    this.data = a;
 
   }
 
@@ -374,19 +549,35 @@ class jsfList {
   * @param index value which will be removed by index
   */
   removeIndex(index) {
+    var a = [];
 
+    this.foreach(function(e, i) {
+      if (i != index) {
+        a.push(e);
+      }
+    });
+
+    this.data = a;
   }
 
   /**
   * @param val returns if list containing element
   */
   contains(val) {
+    var re = false;
     this.foreach(function(e) {
       if(e.toString() == val.toString()) {
-        return true;
+        re = true;
       }
     });
-    return false;
+    return re;
+  }
+
+  /**
+  * @param {numeric} index
+  */
+  index(index) {
+    return this.data[index];
   }
 
   /**
@@ -395,7 +586,7 @@ class jsfList {
   foreach(func) {
     if(typeof func == 'function') {
       for(var i=0; i<this.data.length; i++) {
-        func(this.data[i]);
+        func(this.data[i], i);
       }
     }
   }
--- server.php
+++ server.php
@@ -1,3 +1,14 @@
 <?php
+  if(isset($_POST['action']) && !empty($_POST['action'])) {
+    $action = $_POST['action'];
+    switch($action) {
+          case 'test' : test($_POST);break;
+    }
+  }
+
+  function test($data) {
+    $message = 'Hallo ' . $data['name'] . ' wie geht es dir? Liest Du gerne? Wenn ja habe Ich hier einen Buch Vorschlag für Dich:';
+    echo $message;
+  }
 
 ?>
